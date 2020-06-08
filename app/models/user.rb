67e0fcc01bf9d1be7c46bd92fdc4343174151da1
shota-uchiyama <s.uchiyama25@gmail.com>
2020-06-09 01:37:03 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  validates :nickname, presence: true, length: { maximum: 6 }
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fit: [1800, 1800]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [50, 50]
+    # process resize_to_fit: [300, 300]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
